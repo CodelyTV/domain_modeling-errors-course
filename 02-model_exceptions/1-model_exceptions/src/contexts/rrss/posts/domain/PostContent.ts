e9ea812de59b4e0829dce53709439ccd7fef15c3
Rafa Gómez <rgomezcasas@gmail.com>
2024-04-22 11:52:01 +0200

    feat(2-1): add example

--- 02-model_exceptions/1-model_exceptions/src/contexts/rrss/posts/domain/PostContent.ts
+++ 02-model_exceptions/1-model_exceptions/src/contexts/rrss/posts/domain/PostContent.ts
@@ -3,15 +3,15 @@ import { PostContentIsEmptyError } from "./PostContentIsEmptyError";
 import { PostContentTooLongError } from "./PostContentTooLongError";
 
 export class PostContent extends StringValueObject {
-	public static readonly postMaxLength = 280;
+	public static readonly maxLength = 280;
 
 	constructor(value: string) {
 		if (value.length === 0) {
 			throw new PostContentIsEmptyError();
 		}
 
-		if (value.length > PostContent.postMaxLength) {
-			throw new PostContentTooLongError(value, PostContent.postMaxLength);
+		if (value.length > PostContent.maxLength) {
+			throw new PostContentTooLongError(value, PostContent.maxLength);
 		}
 
 		super(value);
