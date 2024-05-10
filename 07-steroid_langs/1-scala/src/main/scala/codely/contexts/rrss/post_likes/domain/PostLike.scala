7b9576106353feb847ad7a50e4f9b1386435a12d
Rafa Gómez <rgomezcasas@gmail.com>
2024-05-10 11:52:28 +0200

    feat(8-1): add base

--- 07-steroid_langs/1-scala/src/main/scala/codely/contexts/rrss/post_likes/domain/PostLike.scala
+++ 07-steroid_langs/1-scala/src/main/scala/codely/contexts/rrss/post_likes/domain/PostLike.scala
@@ -2,6 +2,13 @@ package codely.contexts.rrss.post_likes.domain
 
 import codely.contexts.shared.domain.DomainEvent
 
+enum PostLikeError:
+	case PostLikeIdTooShort(id: String) extends PostLikeError
+
 case class PostLike(id: String) {
 	def pullDomainEvents(): Seq[PostLikeDomainEvent] = Seq(DomainEvent())
 }
+
+object PostLike:
+	def fromPrimitives(id: String): Either[PostLikeError, PostLike] = PostLike(id)
+end PostLike
